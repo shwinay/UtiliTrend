e0b41a4b9dd07c41050ba313328803750fdf9907
Vraj Patel <patelvap@gmail.com>
2020-01-09 15:04:22 -0500

    finished housing data

--- routes/scripts/housingData.js
+++ routes/scripts/housingData.js
@@ -8,27 +8,41 @@ const fetch = require('node-fetch');
 //const convert = require('xml-js');
 
 let url =
-    `http://www.zillow.com/webservice/GetDeepSearchResults.htm?` +
-    `zws-id=X1-ZWz17gfuapwoi3_61q4u`; // ZWSID
-    //`&address=2114+Bigelow+Ave` + // Address (replace w/ variables later)
-    //`&citystatezip=Seattle%2C+WA`; // Citystatezip (replace w/ variables)
-    
+`http://www.zillow.com/webservice/GetDeepSearchResults.htm?` +
+`zws-id=X1-ZWz17gfuapwoi3_61q4u`; // ZWSID
+//`&address=2114+Bigelow+Ave` + // Address (replace w/ variables later)
+//`&citystatezip=Seattle%2C+WA`; // Citystatezip (replace w/ variables)
+
 function getData(address, citystatezip) {
-
+    
     fetch(url+`&address=${address}&citystatezip=${citystatezip}`)
-        .then(response => response.text())
-        .then(data => {        
-            const taxAssessment = isolateVal('taxAssessment', data);
-            const age = (new Date).getFullYear() - isolateVal('yearBuilt', data);
-            const lotSize = isolateVal('lotSizeSqFt', data);
-            const residenceSize = isolateVal('finishedSqFt', data);
-            const bathrooms = isolateVal('bathrooms', data);
-            const bedrooms = isolateVal('bedrooms', data);
-
-            let temp = data.match('(<amount currency="USD")>([^>]+)(<\/amount)');
-            const zestimate = temp[0].substring(temp[0].indexOf('>') + 1, temp[0].indexOf('</'));
-        })
-        .catch(str => { const dataAsJSON = null; });
+    .then(response => response.text())
+    .then(data => {        
+        const taxAssessment = isolateVal('taxAssessment', data);
+        const age = (new Date).getFullYear() - isolateVal('yearBuilt', data);
+        const lotSize = isolateVal('lotSizeSqFt', data);
+        const residenceSize = isolateVal('finishedSqFt', data);
+        const bathrooms = isolateVal('bathrooms', data);
+        const bedrooms = isolateVal('bedrooms', data);
+        
+        let temp = data.match('(<amount currency="USD")>([^>]+)(<\/amount)');
+        const zestimate = temp[0].substring(temp[0].indexOf('>') + 1, temp[0].indexOf('</'));
+        
+        let obj = {
+            'taxAssessment' : parseInt(taxAssessment),
+            'age' : parseInt(age),
+            'lotSize' : parseInt(lotSize),
+            'residenceSize' : parseInt(residenceSize),
+            'bathrooms' : parseInt(bathrooms),
+            'bedrooms' : parseInt(bedrooms),
+            'zestimate' : parseInt(zestimate),
+        };
+        
+        console.log(obj);
+        
+        return obj;
+    })
+    .catch(str => { const dataAsJSON = null; });
     
 }
 
@@ -38,4 +52,4 @@ function isolateVal(str, data) {
     return match;
 }
 
-getData('2114+Bigelow+Ave', 'Seattle%2C+WA');
+module.exports.getData = getData;
