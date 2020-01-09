6bee99ee3785cbfecb7b785d6e4b8616d1568e47
DESKTOP-ROPIOGL\Ashwin <winkudva@yahoo.com>
2020-01-09 09:33:15 -0600

    set up proxy

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/getbill', function(req, res, next) {
+  res.json(
+  {
+    "something": "something"
+  }
+  );
+});
+
 module.exports = router;
