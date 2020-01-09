2ed0f26f14a2a4d734bcd40b7a9bfefc933cd8be
rickyma9 <48923521+rickyma9@users.noreply.github.com>
2020-01-09 17:30:04 -0500

    added ranges functionality

--- distribution_len/App.js
+++ distribution_len/App.js
@@ -31,6 +31,7 @@ const getInfo = (dataset) => {
   var set = []
   for(var i = 0; i < dataset.length; i++){
     set += dataset[i].name
+    console.log(dataset[i].name)
   }
   return set
 }
@@ -38,13 +39,65 @@ const getInfo = (dataset) => {
 const getCost = (dataset) => {
   var set = []
   for(var i = 0; i < dataset.length; i++){
-    set += dataset[i].avgCost
+    set.push(dataset[i].avgCost)
   }
-  return set
+  return set.sort(function(x, y) {return x - y})
 }
 
+const getRange = (axis, intervalCount) => {
+  var set = []
+  //var res = [lo,hi]
+  var min = axis[0]
+  var max = axis[axis.length - 1]
+  var step = Math.round((max - min) * 100) / (intervalCount * 100)
+  var lo = Math.floor(min)
+  for(var i = 0; i < intervalCount; i++){
+    var tuple = [lo, lo + step]
+    set.push(tuple)
+    lo = lo + step
+  }
+  set[set.length - 1][1] = axis[axis.length - 1]
+return set
+}
+
+function findNumPeopleInRange(data, min, max) {
+  var total = 0;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].avgCost >= min && data[i].avgCost <= max) {
+      console.log(data[i].name + " " + min + "-" + max)
+      total++;
+    }
+  }
+
+  return total
+}
+
+const getNumPeople = (dataset, ranges) => {
+  var set = []
+  for (var i = 0; i < ranges.length; i++) {
+    set.push(findNumPeopleInRange(dataset, ranges[i][0], ranges[i][1]))
+  }
+
+  return set
+} 
+
+function rangeArrayToStringArray(arr) {
+  console.log(arr)
+  var strarr = []
+  for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+    strarr.push(arr[i][0] + "-" + arr[i][1])
+  }
+  console.log(strarr)
+
+  return strarr
+}
+
+console.log(getRange(getCost(dummyData), 5))
+console.log(getCost(dummyData))
+
 const data = {
-  labels: getInfo(dummyData),
+  labels: rangeArrayToStringArray(getRange(getCost(dummyData), 7)),
   datasets: [
     {
       label: 'My First dataset',
@@ -53,7 +106,8 @@ const data = {
       borderWidth: 1,
       hoverBackgroundColor: 'rgba(255,99,132,0.4)',
       hoverBorderColor: 'rgba(255,99,132,1)',
-      data: getCost(dummyData)
+      // data: getCost(dummyData),
+      data : getNumPeople(dummyData, getRange(getCost(dummyData), 7))
     }
   ]
 };
@@ -86,7 +140,14 @@ const App = () => {
             width={100}
             height={300}
             options={{
-              maintainAspectRatio: false
+              maintainAspectRatio: false,
+              scales : {
+                yAxes: [{
+                  ticks : {
+                    beginAtZero: true
+                  }
+                }]
+              }
             }}
           />
         </div>
