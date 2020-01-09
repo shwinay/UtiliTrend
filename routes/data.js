fb65963104567fd0d1f236fc6f83a2ba31241687
DESKTOP-ROPIOGL\Ashwin <winkudva@yahoo.com>
2020-01-09 10:04:46 -0600

    added json template to users.js

---
+++ routes/data.js
@@ -0,0 +1,49 @@
+let data = 
+{
+    users: 
+    {
+        userid: 
+        {
+            name: "name 1",
+            email: "something@yahoo.com",
+            password: "abc123",
+            address: "1234 Something Way",
+            zipcode: "27519",
+            bill: 
+            [
+                {
+                    payee: "McDonald's",
+                    date: "2020-01-04",
+                    amount: "5.14"
+                },
+                {
+                    payee: "Starbucks",
+                    date: "2020-01-02",
+                    amount: "5.55"
+                }
+            ]
+        },
+        
+        userid2: 
+        {
+            name: "name 2",
+            email: "somethingelse@gmail.com",
+            password: "123abc",
+            address: "1234 SomethingElse Way",
+            zipcode: "27519",
+            bill: 
+            [
+                {
+                    payee: "McDonald's",
+                    date: "2020-01-04",
+                    amount: "5.18"
+                },
+                {
+                    payee: "Starbucks",
+                    date: "2020-01-02",
+                    amount: "5.31"
+                }
+            ]
+        }
+    }
+}
