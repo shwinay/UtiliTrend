d76722d6b373cd4a2f96a117f93ad34e60a5dc56
Len Huang <lenhuang720@gmail.com>
2020-01-09 16:59:51 -0500

    Possible sample data

---
+++ distribution_len/sample_graph_data.js
@@ -0,0 +1,12 @@
+export const distributionData = [
+  {
+    name: "Ricky Ma",
+    sq_feet: 300,
+    spending: 16.64
+  },
+  {
+    name: "Len Huang",
+    sq_feet: 500,
+    spending: 27.42
+  }
+];
